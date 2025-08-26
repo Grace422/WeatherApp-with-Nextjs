f2cb2ff6eb5254367adf10698c0c3e3df7a527ac
Grace <neguekwahammaelgrace@gmail.com>
2025-08-26 20:02:17 +0100

    date and time added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import DateFormat from "./DateFormat";
 import Search from "./Search";
 export default function Home() {
   return (
     <div className="border rounded-lg p-5 mx-auto w-1/2 my-20">
       <Search />
+      <DateFormat />
     </div>
   );
 }
